4529d710ef015a6068bb7cd0a6dc14a9f67c5375
Islam Hesham <129389116+IslamHesham-Dev@users.noreply.github.com>
2023-06-08 09:39:29 -0700

    MIPS.asm

--- MIPS.asm
+++ MIPS.asm
@@ -1,141 +1,111 @@
 .data
-	list: .word 3, 2, 10, 0, 1, 2
-	enter: .asciiz "\n"
-.text
-
-
-.globl main
-main:
-	la $a0,list
-	addi $al,$zero,6
-
-	jal Sel_Sort
-	jal Top_rank
-	j END
-
+result:.word
+ 
 Sel_Sort:
-	add $t0 Szero,$zero   #i=0
 
-Loop2:  beq $t0, $a1, exit1   #i==a.length then exit1
+	addi $sp, $sp, -4
+	sw $s0, 0($sp) # int min
+	li $s0, 0      # min = 0
+	li $t0, 0 # i = 0
+  outer_loop:
+  	bge $t0, $a1, goOut
+  	
+  	sll $t2, $t0, 2
+  	add $t2, $t2, $a0
+  	lw $t4, 0($t2)	# a[i]
+  	addi $s0, $t4, 0 # min = a[i]
+  	
+        addi $t3, $t0, 0 # p = i
+  	addi $t1, $t0, 0 # j = i
+  	
+  inner_loop:
+  	bge $t1, $a1, nextIteration
+  	
+  	sll $t5, $t1, 2
+  	add $t5, $t5, $a0
+  	lw $t6, 0($t5) 	# a[j]
+  	
+  	blt $t6, $t4, logic
+  	addi $t1, $t1, 1 # j++
+  	j inner_loop 
+  	logic:
 
-	sll $t1,$t0,2 
-	add $t1,$t1,$a0
-	lw  $t2,0(St1)        #t2=min
+  		move $s0, $t6	# min = a[j]
+  		
+  		addi $t3, $t1, 0  # p = j
+		
+		sll $t7, $t3, 2
+		add $t7, $t7, $a0
+		lw  $t8, 0($t7) # a[p]
+		
+		
+		move $t8, $t4	# a[p] = a[i]
+		move $t4, $s0	# a[i] = min = a[j]
+
+  nextIteration:
+  	addi $t0, $t0, 1
+  	j outer_loop	
+  goOut:
+	addi $sp, $sp, 4
+	lw $s0, 0($sp)	
+	jr $ra
 	
-	add $t3,$t0,$zero     #index=t3
 	
-	add $t4,$t0,$zero     #j=i
-Loop1: 
-	beq $t4,$a1,exit2     #i==a.length then exit2
 	
-	sll $t5,$t4,2
-	add $t5,$t5,$a0
-	lw $t5,0($t5)       #t5=a[j]
+Sem_Abroad:
+    li $t5, 0
+    li $t0, 0 # i = 0
+
+    loopp:
+        beq $a1, $t0, exit_Sem_Abroad
 	
-	slt $t6,$t5,$t2     #if(t5 < t2) then t6 = 1 else t6 = O
-	beq $t6,$zero,cont1
+	sll $t1, $t0, 2
+	add $t1, $t1, $a0
+        lw $t2, 0($t1) # a[i]
+        
+        bge $t2, 70, addValue # a[i] >= 70
+	addi $t0, $0, 1
+        j loopp
+
+    addValue:
+   	sll $t6, $t5,2
+   	add $t6, $t6, $a2
+   	lw $t7, 0($t6)	# result[$t5] 
+        sw $t2, 0($t7) # store a[i] in result array
+        addi $t5, $0, 1
+	j loopp
+
+    exit_Sem_Abroad:
+        jr $ra
+        
 	
-	add $t2,$t5,$zero  #min = a[j]
-	add $t3,$t4,$zero  #index = j
 	
-contl: 
-	addi $t4,$t4,1    #j++
-	j    Loop1
 	
-exit2: 
-	sll $t6,$t0,2
-	add $t6,$t6,$a0
-	lw $t7,0($t6)    #t7=a[i]
 	
-	sll $t8,$t3,2
-	add $t8,$t8,$a0
-	lw $t9,0($t8)    #t9=a[index]
+Top_Rank:
+    li $t0, 0
+    li $v1, 0
+
+loop:
+    beq $t0, $a1, exit
+    lw $t2, 0($a0)
+
+    bgt $t2, $v1, update
+
+    addi $a0, $a0, 4
+    addi $t0, $t0, 1
+    j loop
+
+update:
+    move $v1, $t2
+    addi $a0, $a0, 4
+    addi $t0, $t0, 1
+    j loop
+
+exit:
+    sw $v1, result
+    jr $ra	
 	
-	sw $t7,0($t8)    #store t7 at a[index]
-	
-	sw $t2,0,($t6)   #store t2(min) into a[i]
-	
-	addi $t0,$t0,1   #i++
-	
-	j	Loop2
 	
 exit1:
-	jr $ra
-	
-Top_rank:
-	add $t0,$zero,$zero   #i=0
-	add $v0,$zero,$zero   #max=0
-	
-Loop3:
-	beq $t0,$a1,exit3     #if(i==length)exit the loop
-	
-	sll $t1,$t0,2
-	add $t1,$t1,$a0
-	lw $t1,0($t1)         #t1=a[i]
-	
-	slt $t2,$v0,$t1       #if(max<a[i]) then make max is a[i]
-	beq $t2,$zero,cont2
-	
-	add $v0,$t1,$zero     #max=a[i]
-	
-cont2:
-	addi $t0,$t0,1       #i++
-	j	Loop3
-	
-exit3:
-	jr $ra
-	
-Sem_abroad:
-	add $t0,$zero,$zero	#i=0
-	add $t1,$zero,$zero	#j=0
-	addi $t2,$t2,70		#t2=70
-	
-Loop4:
-	beq $t0,$a1,exit4
-	
-	sll $t3,$t0,2
-	add $t3,$t3,$a0
-	lw $t3,0($t3)           #t3=a[i]
-	
-	slt $t4,$t3,$t2		#if(a[i]<70)
-	beq $t4,$zero,cont3
-	
-	sll $t5,$t1,2
-	add $t5,$t5,$a2         #base address of new array assumed to be in a2
-	sw $t3,0($t5)           #store t3 which is a[i] into address t5
-	addi $t1,$t1,1          #j++
-	
-cont3:
-	addi $t0,$t0,1          #i++
-	j	Loop4
-	
-exit4:
-	jr $ra
 
-print_array:
-	add $s0,$a0,$zero
-	li $v0,1
-	
-	add $s1,$zero,$zero
-	
-L1:
-	beq $s1,$a1,exit5
-	
-	sll $s2,$s1,2
-	add $s2,$s2,$s0
-	lw $a0,0($s2)
-	
-	syscall
-	
-	addi $s1,$s1,1
-	j	L1
-	
-exit5:
-	li $v0,4
-	la $a0,enter
-	syscall
-	
-	add $a0,$s0,$zero
-	jr $ra
-	
-END:
